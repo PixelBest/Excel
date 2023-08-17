8e2eb4a03be94e9d1d0536774995e3b1609892c6
PixelBest <nick.poroschin@yandex.ru>
2023-08-17 08:40:59 +0500

    Добавьте файлы проекта.

---
+++ Excel/DB.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<BaseGuid> Guid { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Excel;Username=postgres;Password=123123");
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BaseGuid>().ToTable("BaseGuid");
+        }
+    }
+    [Table("BaseGuid")]
+    public class BaseGuid
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("Guid")]
+        public string? Guid { get; set; }
+        [Column("DESCRIPTION_TYPE_ID")]
+        public int? DESCRIPTION_TYPE_ID { get; set; }
+        [Column("CODE")]
+        public int? CODE { get; set; }
+        [Column("SHORT_NAME")]
+        public string? SHORT_NAME { get; set; }
+        [Column("NAME")]
+        public string? NAME { get; set; }
+        [Column("BEGIN_DATE")]
+        public string? BEGIN_DATE { get; set; }
+        [Column("END_DATE")]
+        public string? END_DATE { get; set; } 
+        [Column("NOTE")]
+        public string? NOTE { get; set; }
+    }
+}
---
+++ Excel/Program.cs
@@ -0,0 +1,78 @@
+﻿using Excel;
+using ExcelDataReader;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ApplicationContext db = new ApplicationContext();
+using (var stream = File.Open(@"C:\Users\Nikita.Poroshin\Desktop\Full.xlsx", FileMode.Open, FileAccess.Read))
+{
+    Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+    var reader = ExcelReaderFactory.CreateReader(stream, new ExcelReaderConfiguration()
+    {
+        FallbackEncoding = Encoding.GetEncoding(1252),
+
+        Password = "password",
+
+        AutodetectSeparators = new char[] { ',', ';', '\t', '|', '#' },
+
+        LeaveOpen = false,
+
+        AnalyzeInitialCsvRows = 0,
+    });
+
+    var result = reader.AsDataSet();
+
+    var table = result.Tables[0];
+
+    for (int i = 1; i < table.Rows.Count; i++)
+    {
+
+        var row = table.Rows[i].ItemArray;
+        int paketNumberProkat = 0;
+        try
+        {
+            paketNumberProkat = Convert.ToInt32(row[1]);
+        }
+        catch
+        {
+            paketNumberProkat = 0;
+        }
+        int paketNumberProkat1 = 0;
+        try
+        {
+            paketNumberProkat1 = Convert.ToInt32(row[2]);
+        }
+        catch
+        {
+            paketNumberProkat1 = 0;
+        }
+        
+        BaseGuid svpzzd = new BaseGuid()
+        {
+            Guid = Convert.ToString(row[0]),
+            DESCRIPTION_TYPE_ID = paketNumberProkat,
+            CODE = paketNumberProkat1,
+            SHORT_NAME = Convert.ToString(row[3]),
+            NAME = Convert.ToString(row[4]),
+            BEGIN_DATE = Convert.ToString(row[5]),
+            END_DATE = Convert.ToString(row[6]),
+            NOTE = Convert.ToString(row[7]),
+        };
+        if (String.IsNullOrEmpty(svpzzd.SHORT_NAME) == true)
+            svpzzd.SHORT_NAME = "-";
+        if (String.IsNullOrEmpty(svpzzd.NAME) == true)
+            svpzzd.NAME = "-";
+        if (String.IsNullOrEmpty(svpzzd.BEGIN_DATE) == true)
+            svpzzd.BEGIN_DATE = "-";
+        if (String.IsNullOrEmpty(svpzzd.END_DATE) == true)
+            svpzzd.END_DATE = "-";
+        if (String.IsNullOrEmpty(svpzzd.NOTE) == true)
+            svpzzd.NOTE = "-";
+        db.Guid.Add(svpzzd);
+    }
+    db.SaveChanges();
+}
